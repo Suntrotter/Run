fcba98ab8cbeabdb622b09cb8d41fe08ce94c2fa
Suntrotter <suntrotter@gmail.com>
2024-03-12 15:18:01 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Cli Bot assistance for managing contacts and notes
+
+## Steps to start the project:
+
+1. Move to folder that contains file and run command in terminal:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+2. Run main.py
---
+++ src/models/notes.py
@@ -0,0 +1,78 @@
+from collections import UserDict
+import pickle
+import os
+from src.models.note import Note
+
+
+class Notes(UserDict):
+
+    note_id = 1
+    filename = 'notes.bin'
+
+    def save_to_file(self):
+        with open(Notes.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        if os.path.exists(self.filename):
+            with open(Notes.filename, "rb") as file:
+                notes = pickle.load(file)
+                if notes.data.keys():
+                    max_id = max(notes.data.keys())
+                    Notes.note_id = max_id + 1
+                return notes
+        else:
+            return Notes()
+
+    def add_note(self, title: str, content: str, tags: list) -> None: #tags: list of strings
+        pass
+
+
+    def show_notes(self) -> list: #list of dictionaries(to_dict)
+        pass
+    
+
+    def show_note(self, id:str) -> None:
+        pass
+
+
+    def delete_note(self, id:str) -> None:
+        pass
+
+
+    def change_title(self, id:str, title: str) -> None:
+        pass
+    
+
+    def change_content(self, id:str, content: str) -> None:
+        pass
+
+
+    def add_tag(self, id:str, tag: str) -> None:
+        pass
+
+
+    def change_tag(self, id:str, old_tag:str, new_tag:str) -> None:
+        pass
+    
+
+    def find_by_tag(self, tag:str) -> list: #list of dictionaries(to_dict)
+        pass
+
+
+    def find_by_title(self, title:str) -> list: #list of dictionaries(to_dict)
+        pass
+
+
+    def find_by_content(self, content:str) ->  list: #list of dictionaries(to_dict)
+        pass
+    
+
+    def sort_by_tags(self, tags: list) -> list: #list of dictionaries: новий вид списку - подумати як вивести; list of strings
+        pass
+
+
+
+
+
+
